5cd3f07c3764b19d2370633aec06bf21a356101e
cntvc <cntvc@outlook.com>
2022-12-11 21:14:12 +0800

    feat: add data statistics sheet

--- src/genshin/module/gacha/report_gengrator.py
+++ src/genshin/module/gacha/report_gengrator.py
@@ -8,6 +8,7 @@ from typing import List, Optional
 from genshin.config import settings, update_and_save
 from genshin.core import logger
 from genshin.module.gacha.data_struct import (GACHA_QUERY_TYPE_IDS,
+                                              GACHA_QUERY_TYPE_NAMES,
                                               GACHA_TYPE_DICT)
 
 
@@ -54,7 +55,7 @@ class XLSXGenerator(AbstractGenerator):
         logger.debug("创建工作簿: " + workbook_path)
         workbook = Workbook(workbook_path)
 
-        # init title and content format
+        # init format
         content_css = workbook.add_format(
             {
                 "align": "left",
@@ -62,25 +63,44 @@ class XLSXGenerator(AbstractGenerator):
                 "border_color": "#c4c2bf",
                 "bg_color": "#ebebeb",
                 "border": 1,
+                "color": "#8e8e8e",
             }
         )
         title_css = workbook.add_format(
             {
                 "align": "left",
                 "font_name": "微软雅黑",
-                "color": "#757575",
+                "color": "#8e8e8e",
                 "bg_color": "#dbd7d3",
                 "border_color": "#c4c2bf",
                 "border": 1,
                 "bold": True,
             }
         )
-
+        merge_css = workbook.add_format(
+            {
+                "align": "center",
+                "valign": "vcenter",
+                "font_name": "微软雅黑",
+                "color": "#8e8e8e",
+                "bg_color": "#dbd7d3",
+                "border_color": "#c4c2bf",
+                "border": 1,
+                "bold": True,
+            }
+        )
         star_5 = workbook.add_format({"color": "#bd6932", "bold": True})
         star_4 = workbook.add_format({"color": "#a256e1", "bold": True})
         star_3 = workbook.add_format({"color": "#8e8e8e"})
 
-        for gacha_type_id in GACHA_QUERY_TYPE_IDS:
+        overview_sheet = workbook.add_worksheet("数据总览")
+        overview_sheet.set_column("A:E", 20, content_css)
+        overview_sheet.write_row(0, 0, ["项目"] + GACHA_QUERY_TYPE_NAMES, title_css)
+        overview_sheet.write_column(1, 0, ["抽卡总数", "5星出货次数", "出5星平均次数", "保底内抽数"], title_css)
+        END_ROW = 6
+        overview_sheet.merge_range("A{}:E{}".format(END_ROW+1, END_ROW+1), "5星详情", merge_css)
+
+        for cnt, gacha_type_id in enumerate(GACHA_QUERY_TYPE_IDS):
             gacha_type_List = self.data["list"][gacha_type_id][:]
             gacha_type_name = GACHA_TYPE_DICT[gacha_type_id]
 
@@ -90,14 +110,14 @@ class XLSXGenerator(AbstractGenerator):
 
             worksheet.set_column("B:B", 22)
             worksheet.set_column("C:C", 14)
-            worksheet.set_column("F:F", 16)
-
+            worksheet.set_column("F:G", 16)
             worksheet.write_row(0, 0, excel_header, title_css)
 
             worksheet.freeze_panes(1, 0)
 
             total_counter = 0
             pity_counter = 0
+            star_5_list = []
             for gacha in gacha_type_List:
                 time_str = gacha["time"]
                 name = gacha["name"]
@@ -118,6 +138,7 @@ class XLSXGenerator(AbstractGenerator):
                 ]
                 worksheet.write_row(total_counter, 0, excel_data, content_css)
                 if rank_type == 5:
+                    star_5_list.append("{}@{}抽".format(name, pity_counter))
                     pity_counter = 0
 
             first_row = 1  # 不包含表头第一行 (zero indexed)
@@ -145,6 +166,17 @@ class XLSXGenerator(AbstractGenerator):
                 last_col,
                 {"type": "formula", "criteria": "=$E2=3", "format": star_3},
             )
+
+            average_five = "-"
+            if len(star_5_list):
+                average_five = (total_counter - pity_counter) / len(star_5_list)
+            overview_sheet.write_column(
+                1,
+                cnt + 1,
+                [total_counter, len(star_5_list), round(average_five, 2), pity_counter],
+                content_css,
+            )
+            overview_sheet.write_column(END_ROW + 1, cnt + 1, star_5_list)
 
         workbook.close()
         logger.info("XLSX文件写入完成")
